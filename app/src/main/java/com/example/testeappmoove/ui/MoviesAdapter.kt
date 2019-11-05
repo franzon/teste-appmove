e53e4fd270ad86abf0cbe2cc380aacd9d7f0bfcd
Jorge Rossi <jorgefzrossi@gmail.com>
2019-11-05 14:11:13 -0300

    Renderizando primeiro filme de forma diferente dos demais

--- app/src/main/java/com/example/testeappmoove/ui/MoviesAdapter.kt
+++ app/src/main/java/com/example/testeappmoove/ui/MoviesAdapter.kt
@@ -10,20 +10,36 @@ import com.example.testeappmoove.data.Movie
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.movie_item.view.*
 
-class MoviesAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
-        return ViewHolder(view)
-    }
+class MoviesAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<MovieViewHolder>() {
 
     override fun getItemCount() = movies.size
 
-    override fun onBindViewHolder(holder: MoviesAdapter.ViewHolder, position: Int) {
+    override fun getItemViewType(position: Int): Int {
+        if (movies.isEmpty() || position == 0)
+            return 0
+        return 1
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        if (viewType == 0) {
+            val view =
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.first_movie_item, parent, false)
+            return MovieViewHolder(view)
+        }
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        return MovieViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = movies[position]
         holder.title.text = movie.title
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val title = itemView.title
-    }
+}
+
+class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var title = itemView.title
 }
