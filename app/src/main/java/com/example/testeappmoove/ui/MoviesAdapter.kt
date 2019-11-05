6e5f6b7d9f89b4a432bae8f860663e188433fa54
Jorge Rossi <jorgefzrossi@gmail.com>
2019-11-05 15:07:14 -0300

    Estilizando filme em destaque

--- app/src/main/java/com/example/testeappmoove/ui/MoviesAdapter.kt
+++ app/src/main/java/com/example/testeappmoove/ui/MoviesAdapter.kt
@@ -7,8 +7,11 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testeappmoove.R
 import com.example.testeappmoove.data.Movie
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.first_movie_item.view.*
 import kotlinx.android.synthetic.main.movie_item.view.*
+import kotlinx.android.synthetic.main.movie_item.view.title
 
 class MoviesAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<MovieViewHolder>() {
 
@@ -35,11 +38,22 @@ class MoviesAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<Movi
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = movies[position]
-        holder.title.text = movie.title
+        holder.title?.text = movie.title
+        holder.overview?.text = movie.overview
+
+        var imageView = holder?.imageView
+
+        if (imageView != null) {
+            Picasso.get().load("https://image.tmdb.org/t/p/w500/" + movie.backdrop_path)
+                .into(imageView)
+        }
+
     }
 
 }
 
 class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var title = itemView.title
+    var imageView = itemView.imageView
+    var overview = itemView.overview
 }
