b656c9419c46256cff873f35660b54dfe1e014dd
Jorge Rossi <jorgefzrossi@gmail.com>
2019-11-06 22:17:46 -0300

    Renomeando tableName para "liked_movies" para manter consistência

--- app/src/main/java/com/example/testeappmoove/data/dao/LikeDao.kt
+++ app/src/main/java/com/example/testeappmoove/data/dao/LikeDao.kt
@@ -10,4 +10,12 @@ import com.example.testeappmoove.data.entities.LikedMovie
 @Dao
 interface LikeDao {
 
+    @Query("INSERT INTO liked_movies VALUES(:movieId)")
+    fun insert(movieId: Int)
+
+    @Query("DELETE FROM liked_movies WHERE id=:movieId")
+    fun delete(movieId: Int)
+
+    @Query("SELECT * FROM liked_movies")
+    fun all(): LiveData<List<LikedMovie>>
 }
--- app/src/main/java/com/example/testeappmoove/data/entities/LikedMovie.kt
+++ app/src/main/java/com/example/testeappmoove/data/entities/LikedMovie.kt
@@ -3,7 +3,7 @@ package com.example.testeappmoove.data.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "liked_videos")
+@Entity(tableName = "liked_movies")
 data class LikedMovie(
     @PrimaryKey()
     val id: Int
